2dbf009d7202da3621468b504078ad3444ab34b3
Emi Tsukuda <gioramlevi13@gmail.com>
2019-10-21 15:47:51 -0700

    successfully execute till line 2

--- shell.js
+++ shell.js
@@ -24,8 +24,10 @@ prompt.get({
         return 1;
     } else {
         ENVIROMENT_VARIABLES["message"] = result.commitMessage
-        ENVIROMENT_VARIABLES["commit"] = shell.exec("tree `git write-tree`\nparent `git rev-parse HEAD`\nauthor Dan Kozlowski <koz@planetscale.com> 1545187366 +0500\ncommitter Dan Kozlowski <koz@planetscale.com> 1545187366 +0500\n\n${ENVIROMENT_VARIABLES.message}")
-        shell.echo(ENVIROMENT_VARIABLES["commit"])
+        ENVIROMENT_VARIABLES["tree"] = shell.exec(`git write-tree`)
+        ENVIROMENT_VARIABLES["parentTree"] = shell.exec(`git rev-parse HEAD`)
+        ENVIROMENT_VARIABLES["commit"] =`tree ${ENVIROMENT_VARIABLES["tree"]}\nparent ${ENVIROMENT_VARIABLES["parentTree"] }\nauthor Dan Kozlowski <koz@planetscale.com> 1545187366 +0500\ncommitter Dan Kozlowski <koz@planetscale.com> 1545187366 +0500\n\n${ENVIROMENT_VARIABLES["message"]}`
+       console.log( ENVIROMENT_VARIABLES["commit"])
     }
 });
 
